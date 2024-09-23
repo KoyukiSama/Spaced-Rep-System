e2f712c3155a1eb5f01de44c576d6de533e61a52
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-23 15:47:02 +0200

    add displayTasks();

--- front-end/scripts/app.js
+++ front-end/scripts/app.js
@@ -2,8 +2,11 @@ import { getTasks } from "./api";
 
 async function DisplayTasks() {
     try {
-        
+        const tasks = await getTasks();
+        renderTasks(tasks);
     } catch (error) {
         console.error("Error with displaying tasks: ",error);
     }
 }
+
+DisplayTasks();
