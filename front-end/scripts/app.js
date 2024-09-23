ab6acf48572c4e31de3c74c1c2ab7032c350984a
KoyukiSama <119341682+KoyukiSama@users.noreply.github.com.>
2024-09-23 15:46:09 +0200

    add DisplayTasks to main app

---
+++ front-end/scripts/app.js
@@ -0,0 +1,9 @@
+import { getTasks } from "./api";
+
+async function DisplayTasks() {
+    try {
+        
+    } catch (error) {
+        console.error("Error with displaying tasks: ",error);
+    }
+}
